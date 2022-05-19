f101767381140c811945b075308e88d17a64a895
Seven <yaoqi0403@163.com>
2022-05-19 15:40:18 -0400

    Add files via upload

---
+++ GO!/Variable.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func main() {
+	var student1 string = "Qi Yao"
+	student2 := "Felix Qi"
+	x := 2
+	var y int = 4
+
+	fmt.Println(student1)
+	fmt.Println(student2)
+	fmt.Println(x)
+	fmt.Println(y)
+}
