cba281da9d49bf7c72862b74d8637833a18dba66
Seven <yaoqi0403@163.com>
2022-05-20 15:09:30 -0400

    Update 3_ArraySlice.go

--- 3_ArraySlice.go
+++ 3_ArraySlice.go
@@ -27,3 +27,16 @@ func main() {
 func newCard() string {
 	return "Five of Diamonds"
 }
+
+//------------------------Array example--------------
+package main
+
+import "fmt"
+
+func main() {
+	var arr1 = [3]int{1, 2, 3}
+	arr2 := [5]int{1, 2, 3, 4, 5}
+
+	fmt.Println(arr1)
+	fmt.Println(arr2)
+}
