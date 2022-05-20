372d6f0cf892c39b1af763c02f5e2b522e1eb661
Seven <yaoqi0403@163.com>
2022-05-19 22:18:27 -0400

    Update Variable.go

--- GO!/Variable.go
+++ GO!/Variable.go
@@ -13,3 +13,40 @@ func main() {
 	fmt.Println(x)
 	fmt.Println(y)
 }
+//-----------------------------------
+
+package main
+
+import "fmt"
+
+func main() {
+	var a string
+	var b bool
+	var c int
+
+	fmt.Println(a)
+	fmt.Println(b)
+	fmt.Println(c)
+}
+/*These variables are declared but they have not been assigned initial values.
+By running the code, we can see that they already have the default values of their respective types:
+
+a is ""
+b is 0
+c is false
+*/
+//---------------------------------------------------------------
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	var student1 string
+	student1 = "John"
+
+	var student2 string = "Snow"
+	fmt.Print(student1 + " " + student2)
+}
+
