34f0538735823bd5a5d89c396661d1a3364a598e
Seven <yaoqi0403@163.com>
2022-05-20 15:10:37 -0400

    Update 3_ArraySlice.go

--- 3_ArraySlice.go
+++ 3_ArraySlice.go
@@ -36,7 +36,9 @@ import "fmt"
 func main() {
 	var arr1 = [3]int{1, 2, 3}
 	arr2 := [5]int{1, 2, 3, 4, 5}
-
+	arr3 := [...]int{4,5,6,7,8}//This example declares an array of strings:
+	
 	fmt.Println(arr1)
 	fmt.Println(arr2)
+	fmt.Println(arr3)
 }
