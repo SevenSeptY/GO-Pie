b0c9316fb177e61d89eb6ba7559597df996cc287
Seven <yaoqi0403@163.com>
2022-05-25 19:23:30 -0400

    Create mao.go

---
+++ GO!/mao.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+func main() {
+	m1 := make(map[string]string)
+	m1["name"] = "tom"
+	m1["age"] = "20"
+	m1["email"] = "tom@gmail.com"
+
+	fmt.Printf("m1:%v\n", m1)
+
+	var m2 = make(map[string]string)
+	m2 = 
+
+}
