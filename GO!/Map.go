ad81d2121358f106f732091e063cb9eebb3cd3b4
Seven <yaoqi0403@163.com>
2022-05-26 22:56:22 -0400

    Update Map.go

--- GO!/Map.go
+++ GO!/Map.go
@@ -2,15 +2,23 @@ package main
 
 import "fmt"
 
-func main() {
+func test2() {
+	var m2 = map[string]string{"name": "tom", "age": "20", "email": "tom@gmail.com"}
+	fmt.Printf("m2: %v\n", m2)
+}
+
+func test1() {
 	m1 := make(map[string]string)
 	m1["name"] = "tom"
 	m1["age"] = "20"
 	m1["email"] = "tom@gmail.com"
 
-	fmt.Printf("m1:%v\n", m1)
+	fmt.Printf("m1: %v\n", m1)
+	//var m1 map[string]string
+	//m1 = make(map[string]string)
+}
 
-	var m2 = make(map[string]string)
-	m2 = 
-
+func main() {
+	test1()
+	test2()
 }
